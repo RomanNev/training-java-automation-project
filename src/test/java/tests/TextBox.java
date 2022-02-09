da0e3813258fb58d141631cde62fe66cb061849a
RomanGolub <romannovoselovcl@gmail.com>
2022-02-10 00:31:47 +0300

    create training java automation project

---
+++ src/test/java/tests/TextBox.java
@@ -0,0 +1,26 @@
+package tests;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
+
+public class TextBox {
+
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.browserSize = "1920x1080";
+    }
+
+    @Test
+    void successFillTest() {
+        open("/text-box");
+        $(".main-header").shouldHave(text("Text Box"));
+
+
+    }
+
+}
