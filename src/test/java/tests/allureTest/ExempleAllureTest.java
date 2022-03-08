5495c0ec2308ec9a37a546d85b4adbf9d8d407dd
RomanGolub <romannovoselovcl@gmail.com>
2022-03-08 13:46:24 +0300

    aded allure and ListenerTest for github

---
+++ src/test/java/tests/allureTest/ExempleAllureTest.java
@@ -0,0 +1,34 @@
+package tests.allureTest;
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class ExempleAllureTest {
+
+
+    @BeforeAll
+    static void beforeAll() {
+        //Arrange
+        Configuration.browserSize = "1920x1080";
+    }
+
+    @Test
+    void allureTestGithub(){
+        SelenideLogger.addListener("allure", new AllureSelenide()); //подключаем визуализацию шагов в отчете
+
+        open("https://github.com/");
+        $(".header-search-input").setValue("eroshenkoam/allure-example").submit();
+        $(By.linkText("eroshenkoam/allure-example")).click(); //клик по полой ссылке
+        $(By.partialLinkText("Issue")).click();// клик по фрагменту текста  ссылки
+        $(withText("68")).should(Condition.exist);
+
+
+    }
+}
---
+++ src/test/java/tests/allureTest/ListenerTest.java
@@ -0,0 +1,40 @@
+package tests.allureTest;
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class ListenerTest {
+
+    @BeforeAll
+    static void beforeAll() {
+        //Arrange
+        Configuration.browserSize = "1920x1080";
+    }
+
+    @Test
+    void allureTestListener(){
+        SelenideLogger.addListener("allure", new AllureSelenide());
+
+        // открыть github
+        open("https://github.com/");
+        // ввести в поиск RomanNev/training-java-automation-project
+        $(".header-search-input").setValue("RomanNev/training-java-automation-project").submit();
+        // перейти в искомый репозиторий
+        $(By.linkText("RomanNev/training-java-automation-project")).click();
+
+        //искомый репозиторий есть на странице
+        //есть поле Issues
+        $("#repository-container-header").shouldHave(
+                text("training-java-automation-project"),
+                text("Issues"));
+
+
+    }
+
+}
