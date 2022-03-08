4bd2f7287d732c7f58a5d28130c6275026fe2bc3
RomanGolub <romannovoselovcl@gmail.com>
2022-03-08 14:19:17 +0300

    aded LambdaTestsSteps

---
+++ src/test/java/tests/allureTest/LambdaTestsSteps.java
@@ -0,0 +1,46 @@
+package tests.allureTest;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static io.qameta.allure.Allure.step;
+
+public class LambdaTestsSteps {
+
+    @BeforeAll
+    static void beforeAll() {
+        //Arrange
+        Configuration.browserSize = "1920x1080";
+    }
+
+    @Test
+    void allureLambdaTests(){
+        SelenideLogger.addListener("allure", new AllureSelenide());
+
+        step("Открываем страницу github", () ->{
+            open("https://github.com/");
+        });
+
+        step(" ввести в поиск RomanNev/training-java-automation-project", () ->{
+            $(".header-search-input").setValue("RomanNev/training-java-automation-project").submit();
+        });
+
+        step("перейти в искомый репозиторий", () -> {
+            $(By.linkText("RomanNev/training-java-automation-project")).click();
+        });
+
+        step("искомый репозиторий есть на странице и поле Issues есть на странице", () -> {
+            $("#repository-container-header").shouldHave(
+                    text("training-java-automation-project"),
+                    text("Issues"));
+        });
+
+    }
+}
--- src/test/java/tests/allureTest/ListenerTest.java
+++ src/test/java/tests/allureTest/ListenerTest.java
@@ -34,7 +34,6 @@ public class ListenerTest {
                 text("training-java-automation-project"),
                 text("Issues"));
 
-
     }
 
 }
